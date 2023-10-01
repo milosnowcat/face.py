dd5bf7dd608e49e2a3b8b8230c047225d971041a
milosnowcat <ramiro.alvarez@rahcode.com>
2023-10-01 10:32:56 -0600

    TODO

---
+++ main.py
@@ -0,0 +1,4 @@
+# TODO Hacer un sistema de reconocimiento facial
+# donde el usuario pueda poner una imagen
+# con un rostro en una carpeta, el sistema la extraiga
+# y lo compare con los rostros que aparezcan en una cámara
